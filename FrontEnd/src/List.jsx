9223f8335b808e25b847ee72e3ff5fb1c8fe9c51
AMLENDU562 <93076739+AMLENDU562@users.noreply.github.com>
2024-03-31 22:43:50 +0530

    Add files via upload

--- FrontEnd/src/List.jsx
+++ FrontEnd/src/List.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import axios from 'axios'
 function List(props)
@@ -20,4 +21,28 @@ function List(props)
     );
 }
 
+=======
+import React from 'react'
+import axios from 'axios'
+function List(props)
+{
+    function handleClick()
+    {
+        var deleteid=props.deleteId;
+        
+        axios.post("http://localhost:3000/Delete", { deleteid: deleteid })
+        .then(result => console.log(result.data))
+        .catch(e => console.log(e));
+        props.delete(props.id);
+
+    }
+
+    return(
+    <div className=' bg-blue-300 text-2xl w-full'>
+        <li className=' border-4 border-white  border-solid rounded-xl list-none p-3' onClick={handleClick}>{props.text}</li>
+      </div>
+    );
+}
+
+>>>>>>> 41b52bd9a9669270368e55a89cdc47bd25d2e136
 export default List;
